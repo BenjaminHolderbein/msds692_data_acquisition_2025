351976705a74d507e6fd102f4b5a1c8569cccc1a
benjaminholderbein <bgholderbein@dons.usfca.edu>
2025-09-29 10:49:08 -0700

    Update ex01.py

--- Day8/Skeleton/ex01.py
+++ Day8/Skeleton/ex01.py
@@ -1,8 +1,23 @@
 from playwright.sync_api import sync_playwright
+import time
 
 # Step 1. Create a browser
 # Can use chromium/firefox/webkit
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    # time.sleep(5)
 
-# Step 2. Create a new BrowserContext
+    # Step 2. Create a new BrowserContext
+    context = browser.new_context()
+    page = context.new_page()
+    # time.sleep(5)
 
-# Step 3. Open a page
+    # Step 3. Open a page
+    page.goto("https://reddit.com")
+
+    page.wait_for_selector("nav")
+
+    for nav in page.query_selector_all("a"):
+        print(nav.get_attribute("href"))
+
+    browser.close()
